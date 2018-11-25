3b35f7c3e55dfa915f8a402c3576be99015994af
Alex Bal <differencialx@gmail.com>
2018-11-25 22:47:54 +0200

    Initial commit

---
+++ lib/swgem_wrapper.rb
@@ -0,0 +1,11 @@
+require "swgem_wrapper/version"
+require 'swgem'
+require 'faraday'
+require 'json'
+
+module SwgemWrapper
+end
+
+module SWGEM
+  BASE_URL = 'https://swapi.co/api/'.freeze
+end
---
+++ lib/swgem_wrapper/version.rb
@@ -0,0 +1,3 @@
+module SwgemWrapper
+  VERSION = "0.1.0"
+end
